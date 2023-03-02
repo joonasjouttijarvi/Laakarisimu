b61a6badd9a4a60722c9d87781eb27231f739a18
joonas <joonas.jouttijarvi@gmail.com>
2023-03-02 12:46:20 +0200

    cu

--- src/main/java/com/laakarisimu/simu/dao/IDao.java
+++ src/main/java/com/laakarisimu/simu/dao/IDao.java
@@ -1,11 +1,14 @@
 package com.laakarisimu.simu.dao;
 
 import com.laakarisimu.simu.model.Asiakas;
+import entity.Potilaat;
+
+import java.util.List;
 
 public interface IDao {
 
     void lisaaPotilas(Asiakas a);
     void clearDatabase();
-    void getKaikkiPotilaat();
+    List<Potilaat> getKaikkiPotilaat();
     void getPotilasById(int id);
 }
