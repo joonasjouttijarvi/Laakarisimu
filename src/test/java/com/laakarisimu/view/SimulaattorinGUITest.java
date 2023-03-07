ab1e8911cf1e263f6528380e6a780c96615e7d7f
joonas <joonas.jouttijarvi@gmail.com>
2023-03-07 16:32:25 +0200

    Database view

---
+++ src/test/java/com/laakarisimu/view/SimulaattorinGUITest.java
@@ -0,0 +1,394 @@
+package com.laakarisimu.view;
+
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+
+class SimulaattorinGUITest {
+    /**
+     * Method under test: {@link SimulaattorinGUI#start(Stage)}
+     */
+    @Test
+    void testStart() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+        Stage primaryStage = null;
+
+        // Act
+        simulaattorinGUI.start(primaryStage);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#getAika()}
+     */
+    @Test
+    void testGetAika() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+
+        // Act
+        double actualAika = simulaattorinGUI.getAika();
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#getViive()}
+     */
+    @Test
+    void testGetViive() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+
+        // Act
+        long actualViive = simulaattorinGUI.getViive();
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#setLoppuaika(double)}
+     */
+    @Test
+    void testSetLoppuaika() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+        double aika = 0.0d;
+
+        // Act
+        simulaattorinGUI.setLoppuaika(aika);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#setKaikkiPalvellut(int)}
+     */
+    @Test
+    void testSetKaikkiPalvellut() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+        int palvellutAsiakkaat = 0;
+
+        // Act
+        simulaattorinGUI.setKaikkiPalvellut(palvellutAsiakkaat);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#setSairaanhoitajanPalvelemat(int)}
+     */
+    @Test
+    void testSetSairaanhoitajanPalvelemat() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+        int sairaanhoitajanPalvelematAs = 0;
+
+        // Act
+        simulaattorinGUI.setSairaanhoitajanPalvelemat(sairaanhoitajanPalvelematAs);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#setLaakarinPalvelemat(int)}
+     */
+    @Test
+    void testSetLaakarinPalvelemat() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+        int laakarinPalvelematAs = 0;
+
+        // Act
+        simulaattorinGUI.setLaakarinPalvelemat(laakarinPalvelematAs);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#setLaakarinPalkka(double)}
+     */
+    @Test
+    void testSetLaakarinPalkka() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+        double palkka = 0.0d;
+
+        // Act
+        simulaattorinGUI.setLaakarinPalkka(palkka);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#setSairaanhoitajanPalkka(double)}
+     */
+    @Test
+    void testSetSairaanhoitajanPalkka() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+        double palkka = 0.0d;
+
+        // Act
+        simulaattorinGUI.setSairaanhoitajanPalkka(palkka);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#setHoidontarveLieva(double)}
+     */
+    @Test
+    void testSetHoidontarveLieva() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+        double lieva = 0.0d;
+
+        // Act
+        simulaattorinGUI.setHoidontarveLieva(lieva);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#setHoidontarveKohtalainen(double)}
+     */
+    @Test
+    void testSetHoidontarveKohtalainen() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+        double kohtalainen = 0.0d;
+
+        // Act
+        simulaattorinGUI.setHoidontarveKohtalainen(kohtalainen);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#setHoidontarveVakava(double)}
+     */
+    @Test
+    void testSetHoidontarveVakava() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+        double vakava = 0.0d;
+
+        // Act
+        simulaattorinGUI.setHoidontarveVakava(vakava);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#getAsiakkaanSaapumistiheys()}
+     */
+    @Test
+    void testGetAsiakkaanSaapumistiheys() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+
+        // Act
+        double actualAsiakkaanSaapumistiheys = simulaattorinGUI.getAsiakkaanSaapumistiheys();
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#getSairaanhoitajanPalveluaika()}
+     */
+    @Test
+    void testGetSairaanhoitajanPalveluaika() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+
+        // Act
+        double actualSairaanhoitajanPalveluaika = simulaattorinGUI.getSairaanhoitajanPalveluaika();
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#getLaakarinPalveluaika()}
+     */
+    @Test
+    void testGetLaakarinPalveluaika() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+
+        // Act
+        double actualLaakarinPalveluaika = simulaattorinGUI.getLaakarinPalveluaika();
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#getKassanPalveluaika()}
+     */
+    @Test
+    void testGetKassanPalveluaika() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+
+        // Act
+        double actualKassanPalveluaika = simulaattorinGUI.getKassanPalveluaika();
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#setProgress(double)}
+     */
+    @Test
+    void testSetProgress() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+        double progress = 0.0d;
+
+        // Act
+        simulaattorinGUI.setProgress(progress);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#setPalvellutAsiakkaatChart(String, int)}
+     */
+    @Test
+    void testSetPalvellutAsiakkaatChart() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+        String nimi = "";
+        int maara = 0;
+
+        // Act
+        simulaattorinGUI.setPalvellutAsiakkaatChart(nimi, maara);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#setHoidontarveChart(String, double)}
+     */
+    @Test
+    void testSetHoidontarveChart() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+        String nimi = "";
+        double maara = 0.0d;
+
+        // Act
+        simulaattorinGUI.setHoidontarveChart(nimi, maara);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#setHoidonkestoChart(String, int)}
+     */
+    @Test
+    void testSetHoidonkestoChart() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+        String nimi = "";
+        int kesto = 0;
+
+        // Act
+        simulaattorinGUI.setHoidonkestoChart(nimi, kesto);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#setPalkkaChart(String, double)}
+     */
+    @Test
+    void testSetPalkkaChart() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+        String nimi = "";
+        double palkka = 0.0d;
+
+        // Act
+        simulaattorinGUI.setPalkkaChart(nimi, palkka);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#tietokanta()}
+     */
+    @Test
+    void testTietokanta() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+
+        // Act
+        simulaattorinGUI.tietokanta();
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+
+    /**
+     * Method under test: {@link SimulaattorinGUI#clearDatabase()}
+     */
+    @Test
+    void testClearDatabase() {
+        // Arrange
+        // TODO: Populate arranged inputs
+        SimulaattorinGUI simulaattorinGUI = null;
+
+        // Act
+        simulaattorinGUI.clearDatabase();
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+}
+
